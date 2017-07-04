c48b52852a5ec943a47689d51cd14a93bfbbb9c1
WhoIsPeter <ALostBoyFromNeverland@outlook.com>
2017-07-04 16:54:20 -0500

    Added basic bot files

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Commando = require("discord.js-commando");
+const client = new Commando.Client({owner: process.env.OWNER_ID});
+
+client.registry.registerDefaults();
+client.registry.registerCommandsIn(process.cwd() + "/commands");
+
+client.login(process.env.BOT_TOKEN);
