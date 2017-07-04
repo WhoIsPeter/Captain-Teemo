36e88d60017ea46bbd77e63f79eae1cb828ca2c6
WhoIsPeter <ALostBoyFromNeverland@outlook.com>
2017-07-04 18:34:07 -0500

    Added randomchamps command functionality

--- commands/champions/random_champs.js
+++ commands/champions/random_champs.js
@@ -1,4 +1,5 @@
 const Commando = require("discord.js-commando");
+const request = require("request");
 
 class RandomChampsCommand extends Commando.Command {
     constructor(client) {
@@ -10,7 +11,27 @@ class RandomChampsCommand extends Commando.Command {
         });
     }
     async run(message, args) {
-        message.channel.send("Not implemented yet");
+        let memberVoiceChannel = message.member.voiceChannel;
+        if(memberVoiceChannel == undefined) {
+            message.channel.send("You must be in a voice channel in order to use this command", {reply: message.member});
+            return;
+        }
+        let URL = `https://la1.api.riotgames.com/lol/static-data/v3/champions?locale=es_MX&dataById=false&api_key=${process.env.RIOT_API_KEY}`;
+        let champions = [];
+        request(URL, async (error, response, body) => {
+            if(!error && response.statusCode == 200) {
+                let parsed = await JSON.parse(body);
+                for(let element in parsed.data) {
+                    champions.push(parsed.data[element].name);
+                }
+                memberVoiceChannel.members.array().forEach( member => {
+                    let index = Math.floor(Math.random() * 135);
+                    message.channel.send(champions[index], {reply: member});
+                }, this);
+            } else {
+                console.log(error);
+            }
+        });
     }
 }
 
